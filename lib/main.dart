06f5e699b9cb89932952cd683e6800e145f0ab47
Arpit Mohapatra <arpitmohapatra06@gmail.com>
2026-01-02 11:12:35 +0530

    Add Flutter Hooks and Riverpod for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'firebase_options.dart';
 
 
@@ -11,14 +13,16 @@ void main() async {
   );
 
  runApp(
-  const App()
+  const ProviderScope(
+    child: App()
+  )
  );
 }
 
 class App extends StatelessWidget {
   const App({
-    Key? key,
-  }) : super(key: key);
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -33,17 +37,28 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends HookConsumerWidget {
  const HomePage({super.key});
 
  @override
- Widget build(BuildContext context) {
+ Widget build(BuildContext context, WidgetRef ref) {
+  final controller = useTextEditingController(text: "Hooks are working!");
   return Scaffold(
    appBar: AppBar(
     title: const Text('Home Page'),
    ),
-   body: const Center(
-    child: Text('Hello, Flutter!'),
+   body: Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('System Status: Online'),
+        const SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(controller: controller),
+        ),
+      ],
+    ),
    ),
   );
  }
